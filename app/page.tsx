447b71438fe779a54243a90d77ea98823b2f7d2a
wiskani <wiskani@icloud.com>
2024-12-10 16:40:56 -0400

    creando slide trantition

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
-import Navbar from "@/components/Navbar";
 import PageNavigation from "@/components/PageNavigation";
 
 export default function Home() {
     return (
         <>
-            <Navbar/>
             <PageNavigation prevPageLink="./" nextPageLink="./introducing-our-zoo" />
         </>
     );
