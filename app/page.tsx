b2baac782521d340ebaa78a37c26a38bd3b16425
wiskani <wiskani@icloud.com>
2024-12-12 16:40:27 -0400

    making portda

--- app/page.tsx
+++ app/page.tsx
@@ -3,47 +3,40 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="flex bg-black min-h-screen ">
-            <main className="sm:mt-6 lg:mt-8 mt-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-
-                <div
-                    className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
-                    <div className="sm:text-center lg:text-left">
-                        <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
-                            <span className="block xl:inline">BOLETÍN</span>
-                            <span className="block text-indigo-600 xl:inline">Del Sistema de Gestión Ambienta</span>
-                        </h1>
-                        <p
-                            className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                            fugiat veniam occaecat fugiat aliqua.
-                        </p>
-                        {/*-- Button Section--*/}
-                        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                            <div className="rounded-md shadow">
-                                <a href="#"
-                                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600 md:py-4 md:text-lg md:px-10">
-                                    Get started
-                                </a>
-                            </div>
-                            <div className="mt-3 sm:mt-0 sm:ml-3">
-                                <a href="#"
-                                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-800 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                                    Live demo
-                                </a>
-                            </div>
-                        </div>
-                        {/*-- End of Button Section --*/}
-                    </div>
-
-                    {/*--   Image Section  --*/}
-                    <div className="lg:inset-y-0 lg:right-0 lg:w-1/2 my-4">
-                        <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt=""/>
-                    </div>
-                    {/*--   End of Image Section     --*/}
+    <div
+    className="w-full h-screen bg-no-repeat bg-cover bg-center">
+    <div className="w-[90%] mx-auto h-full flex items-center justify-between py-10">
+        <div className="lg:w-fit">
+            <div
+                className="sm:text-6xl xs:text-5xl text-left text-white font-serif font-extrabold uppercase">
+                <h1>Get</h1>
+                <h1>in</h1>
+                <h1 className="bg-black/30 text-white rounded-sm px-1 shadow-sm shadow-white/50">Health</h1>
+                <h1>Today</h1>
+            </div>
+            <div className="w-full flex items-center justify-between mt-6 py-1 px-4 uppercase bg-green-500 rounded-sm">
+                <h3 className="text-white text-lg font-semibold">join now</h3>
+                <div className="w-[40%] flex items-center text-gray-700 text-4xl gap-0">
+                    <hr className="w-full border border-gray-700 relative -right-3" />
                 </div>
-            </main>
-
+            </div>
+            <p className="text-md text-white bg-black/30 font-semibold mt-1 capitalize rounded-lg p-2">25% Discount on first
+                month</p>
         </div>
+
+        <div>
+            <ul className="text-3xl text-white">
+                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full">
+                </li>
+                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full mt-2">
+                </li>
+                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full mt-2">
+                </li>
+                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full mt-2">
+                </li>
+            </ul>
+        </div>
+    </div>
+    </div>
     );
 }
