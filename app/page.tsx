7908ff478e41cda0f3e33ac04b38cb0bd91c14d7
wiskani <wiskani@icloud.com>
2024-12-13 16:54:45 -0400

    making portada

--- app/page.tsx
+++ app/page.tsx
@@ -3,40 +3,31 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-    <div
-    className="w-full h-screen bg-no-repeat bg-cover bg-center">
-    <div className="w-[90%] mx-auto h-full flex items-center justify-between py-10">
-        <div className="lg:w-fit">
-            <div
-                className="sm:text-6xl xs:text-5xl text-left text-white font-serif font-extrabold uppercase">
-                <h1>Get</h1>
-                <h1>in</h1>
-                <h1 className="bg-black/30 text-white rounded-sm px-1 shadow-sm shadow-white/50">Health</h1>
-                <h1>Today</h1>
-            </div>
-            <div className="w-full flex items-center justify-between mt-6 py-1 px-4 uppercase bg-green-500 rounded-sm">
-                <h3 className="text-white text-lg font-semibold">join now</h3>
-                <div className="w-[40%] flex items-center text-gray-700 text-4xl gap-0">
-                    <hr className="w-full border border-gray-700 relative -right-3" />
+        <div
+            className="
+            w-full
+            h-screen
+            bg-no-repeat
+            bg-cover
+            bg-center
+            bg-[url('/images/background_initial.jpg')]
+            ">
+            <div className="w-[90%] mx-auto h-full flex items-center justify-between py-10">
+                <div className="lg:w-fit">
+                    <div
+                        className="text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-left text-white font-adca font-extrabold uppercase">
+                        <h1>BOLETÍN</h1>
+                    </div>
+                    <div
+                        className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-left text-white font-tahu font-extrabold">
+                        <h2 className="text-customRed">Del Sistema de Gestión Ambiental</h2>
+                    </div>
+                    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded shadow-lg transition-all duration-300">
+                        Comenza
+                    </button>
                 </div>
-            </div>
-            <p className="text-md text-white bg-black/30 font-semibold mt-1 capitalize rounded-lg p-2">25% Discount on first
-                month</p>
-        </div>
 
-        <div>
-            <ul className="text-3xl text-white">
-                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full">
-                </li>
-                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full mt-2">
-                </li>
-                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full mt-2">
-                </li>
-                <li className="flex justify-center items-center p-1 bg-black/40 rounded-full mt-2">
-                </li>
-            </ul>
+            </div>
         </div>
-    </div>
-    </div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,46 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        acad: ['var(--font-adca)'],
-        tahu: ['var(--font-tahu)'],
-      },
-      animation: {
-        "slide-left-out": "0.4s ease-in forwards slide-left-out",
-        "slide-left-in": "0.4s ease-out slide-left-in",
-        "slide-right-out": "0.4s ease-in forwards slide-right-out",
-        "slide-right-in": "0.4s ease-out slide-right-in",
-      },
-      keyframes: {
-        "slide-left-out": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                adca: ['var(--font-adca)'],
+                tahu: ['var(--font-tahu)'],
+            },
+            animation: {
+                "slide-left-out": "0.4s ease-in forwards slide-left-out",
+                "slide-left-in": "0.4s ease-out slide-left-in",
+                "slide-right-out": "0.4s ease-in forwards slide-right-out",
+                "slide-right-in": "0.4s ease-out slide-right-in",
+            },
+            keyframes: {
+                "slide-left-out": {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-100%)" },
+                },
+                "slide-left-in": {
+                    from: { transform: "translateX(100%)" },
+                    to: { transform: "translateX(0)" },
+                },
+                "slide-right-out": {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(100%)" },
+                },
+                "slide-right-in": {
+                    from: { transform: "translateX(-100%)" },
+                    to: { transform: "translateX(0)" },
+                },
+            },
+            colors: {
+                customRed: '#FF5A5F',
+            },
         },
-        "slide-left-in": {
-          from: { transform: "translateX(100%)" },
-          to: { transform: "translateX(0)" },
-        },
-        "slide-right-out": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(100%)" },
-        },
-        "slide-right-in": {
-          from: { transform: "translateX(-100%)" },
-          to: { transform: "translateX(0)" },
-        },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
