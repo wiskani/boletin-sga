0b335cedcf1faa145cc391c41e0cdb66c91df997
wiskani <wiskani@icloud.com>
2024-12-27 14:38:28 -0400

    making pag 5

---
+++ content/projects.ts
@@ -0,0 +1,29 @@
+export const projects = [
+    {
+        id: 0,
+        title: "Bahía de rebaje 115/10 kV -115/24.9 kV Subestación Catavi",
+
+        contractors: [
+            {
+                name: "AA MINELCO - 2M",
+                evaluation:
+                {
+                    "Febrero/2023": true,
+                    "Marzo/2023": true,
+                    "Abril/2023": true,
+                    "Mayo/2023": true,
+                    "Junio/2023": true,
+                    "Julio/2023": true,
+                    "Agosto/2023": true,
+                    "Septiembre/2023": true,
+                    "Octubre/2023": true,
+                    "Noviembre/2023": true,
+                    "Diciembre/2023": true,
+                    "Enero/2024": true,
+                    "Febrero/2024": true,
+                    "Marzo/2024": true,
+                },
+            }
+        ],
+    }
+]
