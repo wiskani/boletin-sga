8298508b5b24e4e8fdd0c05161a4b442f522b6cc
wiskani <wiskani@icloud.com>
2024-12-28 00:35:04 -0400

    make count

---
+++ components/CounterNumber/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {motion, useMotionValue, useTransform, animate} from "framer-motion";
+import { useEffect } from "react";
+
+interface CounterNumberProps {
+    initial_value: number;
+    final_value:number;
+}
+
+const CounterNumber: React.FC<CounterNumberProps> =({initial_value, final_value}) => {
+    return null
+}
+
+export default CounterNumber
--- content/projects.ts
+++ content/projects.ts
@@ -3,6 +3,8 @@ export const projects = [
         id: 0,
         title: "Bahía de rebaje 115/10 kV -115/24.9 kV Subestación Catavi",
 
+        point: [-18.410526, -66.575876],
+
         contractors: [
             {
                 name: "AA MINELCO - 2M",
@@ -23,7 +25,71 @@ export const projects = [
                     "Febrero/2024": true,
                     "Marzo/2024": true,
                 },
-            }
+                totalEvaluation: 1,
+            },
         ],
-    }
+        environmentalAwareness: [
+            {
+                name: "Circulación de caminos existentes",
+                "cantidad": 14
+            },
+            {
+                name: "Introducción de Medio Ambiente",
+                "cantidad": 14
+            },
+            {
+                name: "Manejo de fauna",
+                "cantidad": 14
+            },
+            {
+                name: "Manejo de residuos sólidos",
+                "cantidad": 15
+            },
+            {
+                name: "Minimización de generación de polvo",
+                "cantidad": 14
+            },
+            {
+                name: "Prevención de derrames",
+                "cantidad": 13
+            },
+            {
+                name: "Protección de cuerpos de agua",
+                "cantidad": 14
+            },
+            {
+                name: "Protección de flora",
+                "cantidad": 14
+            },
+            {
+                name: "Uso de protectores auditivos",
+                "cantidad": 14
+            },
+        ],
+
+        solidWasted: [
+            {
+                name: "No aprovechables",
+                amount: 49
+            },
+            {
+                name: "Orgánicos",
+                amount: 69
+            },
+            {
+                name: "Residuos Reciclables",
+                amount: 38 
+            },
+
+        ],
+        vehicules: 7,
+
+        wasteWater: {
+            name: "cantidad de conexiones de aguas reciduales",
+            amount: 1
+        }
+        
+
+
+    },
 ]
