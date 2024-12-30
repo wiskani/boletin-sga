5f1859729202774404e1658d03a8174f4169559b
wiskani <89934940+wiskani@users.noreply.github.com>
2024-12-30 11:41:32 -0400

    project 2

--- content/projects.ts
+++ content/projects.ts
@@ -73,4 +73,74 @@ export const projects = [
 
 
     },
+    {
+        id: 1,
+        title: "Línea subterránea 115 kV Kenko - Villa Santiago - Alto La Paz",
+        point: [],
+        line: [],
+        contractors: 2,
+        totalEvaluation: 100,
+        environmentalAwareness: [
+            {
+                name: "Circulación de caminos existentes",
+                "cantidad": 32,
+            },
+            {
+                name: "Introducción de Medio Ambiente",
+                "cantidad": 31,
+            },
+            {
+                name: "Manejo de fauna",
+                "cantidad": 32,
+            },
+            {
+                name: "Manejo de residuos sólidos",
+                "cantidad": 35,
+            },
+            {
+                name: "Minimización de generación de polvo",
+                "cantidad": 33,
+            },
+            {
+                name: "Prevención de derrames",
+                "cantidad": 33,
+            },
+            {
+                name: "Protección de cuerpos de agua",
+                "cantidad": 38,
+            },
+            {
+                name: "Protección de flora",
+                "cantidad": 39,
+            },
+            {
+                name: "Uso de protectores auditivos",
+                "cantidad": 37,
+            },
+        ],
+
+        solidWasted: [
+            {
+                name: "No aprovechables",
+                amount: 238, 
+            },
+            {
+                name: "Orgánicos",
+                amount: 310 
+            },
+            {
+                name: "Residuos Reciclables",
+                amount: 138 
+            },
+
+        ],
+        vehicules: 13,
+
+        wasteWater: {
+            name: "cantidad de conexiones de aguas reciduales",
+            amount: 2
+        }
+
+
+    }
 ]
