efca8fbe5d95e76611b689686dff298218e9024d
wiskani <wiskani@icloud.com>
2024-12-30 16:47:12 -0400

    making back cover

--- app/backcover/page.tsx
+++ app/backcover/page.tsx
@@ -1,6 +1,14 @@
+"use client"
+ 
+import Image from "next/image"
+import { PiKeyReturnFill } from "react-icons/pi";
+import { useRouter } from "next/navigation";
+
 export default function Page() {
+    const router = useRouter();
+
     return (
-     <div
+        <div
             className="
             relative
             w-full
@@ -8,16 +16,66 @@ export default function Page() {
             bg-no-repeat
             bg-cover
             bg-center
-            bg-[url('/images/background_initial.jpg')]
-            filter
-            " // Aplicando blanco y negro y desenfoque
-            style={{
-                filter: 'grayscale(1)',
-            }}
+            bg-[url('/images/background_back_cover.jpg')]
+            " 
         >
-            <p
-                className="text-red-50"
-            >hola</p>
+            <div
+                className="
+                pt-20
+                grid
+                gap-6
+                grid-cols-1
+                md:grid-cols-2
+                "
+            >
+                <div
+                    className="flex flex-col m-5 space-y-7 items-center justify-center"
+                >
+                    <Image
+                        src="/images/logo_white_full.png"
+                        alt="recomendación"
+                        width={310}
+                        height={310}
+
+                    />
+                </div>
+                <div
+                    className="flex flex-col m-5 space-y-5 items-center justify-center"
+                >
+                    <p
+                        className="text-sm font-stemligth md:text-lg"
+                    >
+                        Una empresa filial de
+                    </p>
+
+                    <Image
+                        src="/images/logo_corporacion.png"
+                        alt="recomendación"
+                        width={280}
+                        height={280}
+
+                    />
+                </div>
+            </div>
+            <div
+                className="flex flex-col m-5 space-y-7 items-center justify-center"
+            >
+                <p
+                    className="text-center text-sm font-stemligth md:text-lg"
+                >
+                    Calle Walter Galindo No S-3643, entre calles Hernan Muller y Aranibar Orozco <br />
+                    (Av. Victor Ustáriz km 4 1⁄2). Casilla No 640<br/>
+                    Teléfonos: (591-4) 425900 - 4259512<br/>
+                    Fax: (591-4) 4259516<br/>
+                    E-mail: endetransmision@endetransmision.bo<br/>
+                    Cochabamba - Bolivia<br/>
+                </p>
+                <PiKeyReturnFill
+                    className="text-3xl md:text-5xl cursor-pointer hover:text-blue-700"
+                    onClick={()=>router.push("/")}
+                />
+
+            </div>
         </div>
     )
 };
